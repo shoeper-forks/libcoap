ff6a35e8ca388d00ea04af5f8e36505ef304d8e2
Olaf Bergmann <bergmann@tzi.org>
2013-07-26 15:53:34 +0200

    helper macro for memory allocation/release

--- mem.h
+++ mem.h
@@ -1,7 +1,7 @@
 /* mem.h -- CoAP memory handling
  *          Currently, this is just a dummy for malloc/free
  *
- * Copyright (C) 2010,2011 Olaf Bergmann <bergmann@tzi.org>
+ * Copyright (C) 2010--2013 Olaf Bergmann <bergmann@tzi.org>
  *
  * This file is part of the CoAP library libcoap. Please see
  * README for terms of use. 
@@ -14,5 +14,9 @@
 
 #define coap_malloc(size) malloc(size)
 #define coap_free(size) free(size)
+
+#define COAP_MALLOC_TYPE(Type) \
+  ((coap_##Type##_t *)coap_malloc(sizeof(coap_##Type##_t)))
+#define COAP_FREE_TYPE(Type, Object) coap_free(Object)
 
 #endif /* _COAP_MEM_H_ */
